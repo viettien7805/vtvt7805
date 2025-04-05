e1444e0cce5773e3f8d38103f26563c2eccd4250
Võ Trần Việt Tiến <viettien7805@gmail.com>
2025-04-05 17:00:22 +0700

    Update API URL for Render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
     // Thêm todo
     const addTodo = () => {
-        fetch('http://localhost:5000/todos', {
+        fetch('https://vtvt7805.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ task })
